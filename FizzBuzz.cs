67d8df63225f8403fc226693eac763723f109a2f
Sarah Powell <slhpowell@gmail.com>
2012-02-22 11:37:04 -0500

    newest attempt, 10min

---
+++ FizzBuzz.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzzKata5
+{
+	class FizzBuzz
+	{
+		public bool IsDivisibleByThree(int p)
+		{
+			return (p % 3) == 0;
+		}
+
+		public bool IsDivisibleByFive(int p)
+		{
+			return (p % 5) == 0;
+		}
+
+		public string GenerateString(int p)
+		{
+			if (IsDivisibleByFive(p) && IsDivisibleByThree(p))
+			{
+				return "FizzBuzz";
+			}
+			else if (IsDivisibleByThree(p))
+			{
+				return "Fizz";
+			}
+			else if (IsDivisibleByFive(p))
+			{
+				return "Buzz";
+			}
+			else
+			{
+				return p.ToString();
+			}
+		}
+
+		public string GenerateOutput(int minimum, int maximum)
+		{
+			StringBuilder sb = new StringBuilder();
+			for (int i = minimum; i <= maximum; i++)
+			{
+				sb.AppendFormat("{0} ", GenerateString(i));
+			}
+			return sb.ToString();
+		}
+	}
+}
---
+++ FizzBuzzTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace FizzBuzzKata5
+{
+	[TestFixture]
+	public class FizzBuzzTests
+	{
+		[Test]
+		public void ThreeIsDivisibleByThreeTest()
+		{
+			FizzBuzz fb = new FizzBuzz();
+
+			bool isDivisibleByThree = fb.IsDivisibleByThree(3);
+
+			Assert.That(isDivisibleByThree, Is.True);
+		}
+
+		[Test]
+		public void FiveIsDivisibleByFiveTest()
+		{
+			FizzBuzz fb = new FizzBuzz();
+
+			bool isDivisibleByFive = fb.IsDivisibleByFive(5);
+
+			Assert.That(isDivisibleByFive, Is.True);
+		}
+
+		[Test]
+		public void IntegerDivisibleByThreeReturnsFizzTest()
+		{
+			FizzBuzz fb = new FizzBuzz();
+
+			string integerWord = fb.GenerateString(3);
+
+			Assert.That(integerWord, Is.EqualTo("Fizz"));
+		}
+
+		[Test]
+		public void IntegerDivisibleByFiveReturnsBuzzTest()
+		{
+			FizzBuzz fb = new FizzBuzz();
+
+			string integerWord = fb.GenerateString(5);
+
+			Assert.That(integerWord, Is.EqualTo("Buzz"));
+		}
+
+		[Test]
+		public void IngeterDivisibleByThreeAndFiveReturnsFizzBuzzTest()
+		{
+			FizzBuzz fb = new FizzBuzz();
+
+			string integerWord = fb.GenerateString(15);
+
+			Assert.That(integerWord, Is.EqualTo("FizzBuzz"));
+		}
+
+		[Test]
+		public void IntegerNotDivisibleByThreeOrFiveReturnsItselfTest()
+		{
+			FizzBuzz fb = new FizzBuzz();
+
+			string integerWord = fb.GenerateString(11);
+
+			Assert.That(integerWord, Is.EqualTo("11"));
+		}
+
+		[Test]
+		public void CanGenerateExpectedOutputTest()
+		{
+			FizzBuzz fb = new FizzBuzz();
+
+			int minimum = 1;
+			int maximum = 15;
+
+			string output = fb.GenerateOutput(minimum, maximum);
+
+			Assert.That(output, Is.EqualTo("1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz "));
+		}
+
+	}
+}
